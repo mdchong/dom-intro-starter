05ada26d13a484e771afc162c88f8d37ed866e4c
mdchong <mdchong@asu.edu>
2025-02-25 14:41:27 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,3 +8,24 @@ for (let image of images) {
 }
 
 // third section
+let lightBlueSpans = document.getElementsByClassName("light-blue");
+let dkBlueSpans = document.getElementsByClassName("med-blue");
+
+for (let span of lightBlueSpans) {
+    span.style.color = "#7C9EA6";
+}
+
+for (let span of dkBlueSpans) {
+    span.style.color = "#3C5E73";
+}
+
+// fourth section
+let svgs = document.querySelectorAll("#svgs svg");
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+for (let i = 0; i < svgs.length; i++) {
+    svgs[i].style.stroke = colors[i];
+}
+
+// fifth section
+document.querySelector("#query .bold").style.color = "#7C9EA6";
