3ad453975e863da2b6e4fde1bd03045b94b34071
mdchong <mdchong@asu.edu>
2025-02-25 14:24:38 -0700

    .

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// first session
+document.getElementById("firstPara").innerHTML += "<span class='tan'> This is new tan text</span>";
+
+// second session
+let images = document.getElementsByTagName("img");
+for (let image of images) {
+    image.width = "250";
+}
+
+// third section
